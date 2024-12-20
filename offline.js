25234fc449fc0a260414cbe8a593f9ae54349a13
Pasindu Promod <dev@ninehermits.com>
2024-12-20 17:36:41 +0530

    aa

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1734693418,
+	"version": 1734696382,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -9,7 +9,9 @@
 		"images/bgloop-sheet0.png",
 		"images/memesnake-sheet0.png",
 		"images/coin-sheet0.png",
-		"images/sprite-sheet0.png",
+		"images/memesnake2-sheet0.png",
+		"images/trigger1-sheet0.png",
+		"images/sprite2-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
