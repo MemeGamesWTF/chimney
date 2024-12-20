bf1b5caff10a8f3d1349f368456bf78890a8d0e0
Pasindu Promod <dev@ninehermits.com>
2024-12-20 16:18:41 +0530

    first commit

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1734691648,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/assetsmemecube-sheet0.png",
+		"images/bgloop-sheet0.png",
+		"images/memesnake-sheet0.png",
+		"images/coin-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
